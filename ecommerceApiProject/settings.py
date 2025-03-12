005e1b313d083bdbbde447af222c954716dcde6a
CodeWithClinton <clintonmatics@gmail.com>
2025-03-12 04:33:56 +0100

    postgres db

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -102,7 +102,23 @@ WSGI_APPLICATION = 'ecommerceApiProject.wsgi.application'
 #     }
 
 
-# DATABASES = {
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'railway',
+            'USER': 'postgres',
+            'PASSWORD': "nIVmTssiiqRURwFENJIUAncraumoAAya",
+            'HOST': "metro.proxy.rlwy.net",
+            'PORT': "22806",  
+        }
+    }
+
+
+
+# DB = os.getenv("DB", "False").lower() in ["true", "1"]
+
+# if DB:
+#     DATABASES = {
 #         'default': {
 #             'ENGINE': 'django.db.backends.postgresql',
 #             'NAME': 'railway',
@@ -113,29 +129,13 @@ WSGI_APPLICATION = 'ecommerceApiProject.wsgi.application'
 #         }
 #     }
 
-
-
-DB = os.getenv("DB", "False").lower() in ["true", "1"]
-
-if DB:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'railway',
-            'USER': 'postgres',
-            'PASSWORD': "nIVmTssiiqRURwFENJIUAncraumoAAya",
-            'HOST': "metro.proxy.rlwy.net",
-            'PORT': "22806",  
-        }
-    }
-    
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
    
 
