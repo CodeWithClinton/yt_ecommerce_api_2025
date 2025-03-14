da4dbb87da64a8bacf154913c1e07543804aab34
CodeWithClinton <clintonmatics@gmail.com>
2025-03-14 01:02:53 +0100

    adjusted databse

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -91,30 +91,45 @@ WSGI_APPLICATION = 'ecommerceApiProject.wsgi.application'
 #     }
 
 
+DB = os.getenv("DB", "False").lower() in ["true", "1", "2"]
+
+if DB:
+
+    DATABASES = {
+            'default': {
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'railway',
+                'USER': 'postgres',
+                'PASSWORD': "nIVmTssiiqRURwFENJIUAncraumoAAya",
+                'HOST': "metro.proxy.rlwy.net",
+                'PORT': "22806",  
+            }
+        }
+
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
+
+
+
 
 # DATABASES = {
 #         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
 #             'NAME': 'railway',
 #             'USER': 'postgres',
-#             'PASSWORD': "nIVmTssiiqRURwFENJIUAncraumoAAya",
-#             'HOST': "metro.proxy.rlwy.net",
-#             'PORT': "22806",  
+#             'PASSWORD': os.getenv("PG_PASSWORD"),
+#             'HOST': os.getenv("PG_HOST"),
+#             'PORT': os.getenv("PG_PORT"),
 #         }
 #     }
-
-
-
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'railway',
-            'USER': 'postgres',
-            'PASSWORD': os.getenv("PG_PASSWORD"),
-            'HOST': os.getenv("PG_HOST"),
-            'PORT': os.getenv("PG_PORT"),  
-        }
-    }
 
 
    
