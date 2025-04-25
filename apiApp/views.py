e16aaec28c73bfb0409604f49a50a2d4c2f27083
CodeWithClinton <clintonmatics@gmail.com>
2025-04-25 17:32:06 +0100

    added vat fee to stripe's payment

--- apiApp/views.py
+++ apiApp/views.py
@@ -258,7 +258,7 @@ def my_webhook_view(request):
 def fulfill_checkout(session, cart_code):
     
     order = Order.objects.create(stripe_checkout_id=session["id"],
-        amount=session["amount_total"],
+        amount=int(session["amount_total"])/100,
         currency=session["currency"],
         customer_email=session["customer_email"],
         status="Paid",)
