f63619d0ff6ccf2e9ba5cb5be9ef0f395295c2f4
CodeWithClinton <clintonmatics@gmail.com>
2025-04-25 17:19:08 +0100

    added vat fee to stripe's payment

--- apiApp/views.py
+++ apiApp/views.py
@@ -259,10 +259,14 @@ def fulfill_checkout(session, cart_code):
     
     order = Order.objects.create(stripe_checkout_id=session["id"],
         amount=session["amount_total"],
-        # amount = session["amount_total"] / 100
         currency=session["currency"],
         customer_email=session["customer_email"],
         status="Paid",)
+    
+    if order:
+        print("yeehhhhh")
+    else: 
+        print("Nayyyyy")
     
 
     cart = Cart.objects.get(cart_code=cart_code)
